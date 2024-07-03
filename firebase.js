52dff467f783fd5d9691bccade43bd318978f6dd
Abdurrahman <dreydare16@gmail.com>
2024-07-03 13:06:17 +0100

    renamed env var

--- firebase.js
+++ firebase.js
@@ -10,13 +10,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_SWIFTCART_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_SWIFTCART_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_SWIFTCART_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_SWIFTCART_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env
-    .VITE_SWIFTCART_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_SWIFTCART_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_HAVVA_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_HAVVA_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_HAVVA_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_HAVVA_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_HAVVA_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_HAVVA_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
